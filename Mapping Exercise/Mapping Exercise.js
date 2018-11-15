99a8889e99ad6ce370e9797e914d74892c7caffb
(s) Sam Jackson 1 <sjackson5@pm7-0067.uopnet.plymouth.ac.uk>
2018-11-15 11:02:38 +0000

    color variable

--- Mapping Exercise/Mapping Exercise.js
+++ Mapping Exercise/Mapping Exercise.js
@@ -9,18 +9,18 @@ function draw() {
   var y1 = map(mouseY, 0, 500, 0, 255);
   var x2 = map(mouseX, 0, 500, 255, 0);
   var y2 = map(mouseY, 0, 500, 255, 0);
-  var c1 = (255, 0, 0, x1);
-  var c2 = (0, 0, 255, y1);
-  var c3 = (0, 255, 0, x2);
-  var c4 = (255, 255, 0, y2);
+  var c1 = color(255, 0, 0, x1);
+  var c2 = color(0, 0, 255, y1);
+  var c3 = color(0, 255, 0, x2);
+  var c4 = color(255, 255, 0, y2);
   background(0);
-  fill(c1, 0, 0);
+  fill(c1);
   rect(0, 0, 250, 250);
-  fill(0, 0, c2);
+  fill(c2);
   rect(250, 250, 250, 250);
-  fill(0, c3, 0);
+  fill(c3);
   rect(250, 0, 250, 250);
-  fill(c4, c4, 0);
+  fill(c4);
   rect(0, 250, 250, 250);
 
 }
