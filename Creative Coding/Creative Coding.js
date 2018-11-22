0e26736f3b357a8a2a32d2bb18801dcee8b05d80
(s) Sam Jackson 1 <sjackson5@pm7-0071.uopnet.plymouth.ac.uk>
2018-11-22 12:16:17 +0000

    Pretty stuff

--- Creative Coding/Creative Coding.js
+++ Creative Coding/Creative Coding.js
@@ -3,15 +3,32 @@ function setup() {
   frameRate(60);
   createCanvas(1000, 600);
   background(0);
+  r = random(255);
+  g = random(255);
+  b = random(255);
 }
 
 function draw() {
-  variableEllipse(mouseX, mouseY, pmouseX, pmouseY);
+  if(mouseIsPressed) {
+    variableEllipse(mouseX, mouseY, pmouseX, pmouseY);
+  }
+}
+function keyPressed() {
+  if(keyCode === LEFT_ARROW) {
+    clear();
+    r = random(255);
+    g = random(255);
+    b = random(255);
+    background(r, g, b);
+  }
 }
 
 function variableEllipse(x, y, px, py) {
   var speed = abs(x-px) + abs(y-py);
-  stroke(speed);
-  fill(random(255), random(255), random(255));
+  stroke(10);
+  r = random(255);
+  g = random(255);
+  b = random(255);
+  fill(r, g, b);
   ellipse(x, y, speed, speed);
 }
